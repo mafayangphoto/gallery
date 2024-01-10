9023de42b6790b0749335e258e48136759f4c4b6
mafayangphoto <mafayangphoto@gmail.com>
2024-01-10 15:08:06 +0800

    wynnie_update

--- src/app/(pages)/Home/page.tsx
+++ src/app/(pages)/Home/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 function page() {
   return (
-    <div>Home</div>
+    <div className='w-full'><Image loading='lazy' placeholder='blur' src={require(`./images/IMG_0515.jpg`)} alt={`home`}/></div>
   )
 }
 
