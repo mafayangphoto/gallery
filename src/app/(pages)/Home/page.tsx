e88f941f52cc07996cacfbbfc74753414d7997d6
mafayangphoto <mafayangphoto@gmail.com>
2024-01-15 23:34:04 +0800

    wynnie_fix_window_in_/Home

--- src/app/(pages)/Home/page.tsx
+++ src/app/(pages)/Home/page.tsx
@@ -1,11 +1,14 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Image from 'next/image';
 
 function page() {
   const [runMotion,setRunMotion] = useState(false);
   const [motionEnd,setMotionEnd] = useState(false);
   const [originDisappear,setOriginDisappear] = useState(false);
+  const observer = useRef<ResizeObserver | null>(null);
+  const windowRef = useRef<HTMLDivElement | null>(null);
+  const [ currentSize, setCurrentSize ] = useState({currentWidth:0,currentHeight:0})
 
   useEffect(() => {
     setTimeout(() => {
@@ -27,15 +30,15 @@ function page() {
 
   return (
     <>
-    <div className='w-full h-full hidden sm:block'>
+    {/* <div className='w-full h-full hidden sm:block' ref={windowRef}>
        <Image className={`${!motionEnd && 'hidden'}`} loading='lazy' placeholder='empty' src={require(`./images/IMG_0515.jpg`)} alt={`home`}/>
-      <div className={`Buy_me_drink ${originDisappear && 'hidden'}`} style={{maxWidth:`${window.innerWidth}px`,overflow:'hidden'}}>
+      <div className={`Buy_me_drink ${originDisappear && 'hidden'}`} style={{maxWidth:`${currentSize.currentWidth}px`,overflow:'hidden'}}>
         <figure className={`${runMotion && 'hover'}`}>
-          <Image className={`${runMotion && 'hover'}`} style={{height:`${window.innerHeight}px`}} src={require(`./images/IMG_0515.jpg`)} alt={`home`}/>
+          <Image className={`${runMotion && 'hover'}`} style={{height:`${currentSize.currentHeight}px`}} src={require(`./images/IMG_0515.jpg`)} alt={`home`}/>
         </figure>
       </div>
-    </div>
-    <div className='w-full h-full flex flex-col sm:hidden'>
+    </div> */}
+    <div className='w-full h-full hidden sm:block'>
       <Image loading='lazy' placeholder='empty' src={require(`./images/IMG_0515.jpg`)} alt={`home`}/>
       <div className='grow'><div className='bg-[#000]'></div></div>
     </div>
