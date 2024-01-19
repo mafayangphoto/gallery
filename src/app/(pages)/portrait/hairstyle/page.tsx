5215cc7e60b0e527ee3b4c8b22ee7c83a966037a
mafayangphoto <mafayangphoto@gmail.com>
2024-01-19 12:00:21 +0800

    wynnie_update_hair_page

---
+++ src/app/(pages)/portrait/hairstyle/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Image from 'next/image';
+import { hairImages } from './imagesLib';
+
+function page() {
+  return (
+    <div className='w-full flex px-2.5 flex-col sm:flex-row'>
+        <div className='flex w-full sm:w-2/5'>
+            <div >
+                {hairImages.blockOne.map((img,idx:number) => {
+                    return(
+                        <div className='p-2.5' key={idx}><Image loading='lazy' placeholder='blur' src={require(`${img.url}`)} alt={`one${idx}`}/></div>
+                    )
+                })}
+                
+            </div>
+        </div>
+        <div className='flex w-full sm:w-2/5'>
+            <div >
+                {hairImages.blockTwo.map((img,idx:number) => {
+                    return(
+                        <div className='p-2.5' key={idx}><Image loading='lazy' placeholder='blur' src={require(`${img.url}`)} alt={`two${idx}`}/></div>
+                    )
+                })}
+                
+            </div>
+        </div>
+        <div className='flex w-full sm:w-2/5'>
+            <div >
+                {hairImages.blockThree.map((img,idx:number) => {
+                    return(
+                        <div className='p-2.5' key={idx}><Image loading='lazy' placeholder='blur' src={require(`${img.url}`)} alt={`three${idx}`}/></div>
+                    )
+                })}
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
