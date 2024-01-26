e1ae813e9fcdcb17258c5443e159015fbc8b5693
mafayangphoto <mafayangphoto@gmail.com>
2024-01-26 14:41:00 +0800

    wynnie_change_to_avif_wedding

--- src/app/(pages)/event/wedding/imagesLib.ts
+++ src/app/(pages)/event/wedding/imagesLib.ts
@@ -3,115 +3,115 @@ import React from "react";
 export const portalWeddingImages = [
     [
         {
-            url:'./images/2W9A6509A.jpg',
+            url:'./images/2W9A6509A.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/2W9A6172.jpg',
+            url:'./images/2W9A6172.avif',
             size:'vertical'
         },
         {
-            url:'./images/2W9A6388.jpg',
+            url:'./images/2W9A6388.avif',
             size:'horizontal'
         },
         {
-            url:'./images/2W9A6570A.jpg',
+            url:'./images/2W9A6570A.avif',
             size:'horizontal'
         },
         {
-            url:'./images/2W9A7538.jpg',
-            size:'vertical'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A6598.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/2W9A6692.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A6751.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A6873.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/2W9A8053.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A7579.jpg',
-            size:'vertical'
-        },
-        {
-            url:'./images/2W9A7685.jpg',
-            size:'vertical'
-        },
-        {
-            url:'./images/nkwo2W9A0828.jpg',
+            url:'./images/2W9A7538.avif',
             size:'vertical'
         },
     ],
     [
         {
-            url:'./images/2W9A8578A.jpg',
+            url:'./images/2W9A6598.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/2W9A6692.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/nkwo2W9A0848.jpg',
+            url:'./images/2W9A6751.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/2W9A6873.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/2W9A8053.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/2W9A7579.avif',
             size:'vertical'
         },
         {
-            url:'./images/nkwo2W9A1218A.jpg',
-            size:'horizontal'
+            url:'./images/2W9A7685.avif',
+            size:'vertical'
         },
         {
-            url:'./images/nkwo2W9A2290.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/nkwo2W9A0883.jpg',
+            url:'./images/nkwo2W9A0828.avif',
             size:'vertical'
         },
     ],
     [
         {
-            url:'./images/nkwo2W9A3083.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/nkwo2W9A3103.jpg',
+            url:'./images/2W9A8578A.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/nkwo2W9A3540.jpg',
+            url:'./images/nkwo2W9A0848.avif',
+            size:'vertical'
+        },
+        {
+            url:'./images/nkwo2W9A1218A.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/nkwo2W9A2290.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/nkwo2W9A0883.avif',
+            size:'vertical'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A3083.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/nkwo2W9A3103.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/nkwo2W9A3869.jpg',
+            url:'./images/nkwo2W9A3540.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A3869.avif',
             size:'horizontal'
         },
         {
-            url:'./images/nkwoIMG_9998.jpg',
+            url:'./images/nkwoIMG_9998.avif',
             size:'horizontal'
         },
     ],
@@ -120,113 +120,113 @@ export const portalWeddingImages = [
 export const mobileWeddingImages = [
     [
         {
-            url:'./images/2W9A6509A.jpg',
+            url:'./images/2W9A6509A.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/2W9A6172.jpg',
+            url:'./images/2W9A6172.avif',
             size:'vertical'
         },
         {
-            url:'./images/2W9A7538.jpg',
+            url:'./images/2W9A7538.avif',
             size:'vertical'
         },
         {
-            url:'./images/2W9A7579.jpg',
-            size:'vertical'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A6388.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/2W9A6570A.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A6751.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/nkwo2W9A0828.jpg',
-            size:'vertical'
-        },
-        {
-            url:'./images/2W9A7685.jpg',
-            size:'vertical'
-        },
-        {
-            url:'./images/nkwo2W9A0848.jpg',
+            url:'./images/2W9A7579.avif',
             size:'vertical'
         },
     ],
     [
         {
-            url:'./images/2W9A6598.jpg',
+            url:'./images/2W9A6388.avif',
             size:'horizontal'
         },
         {
-            url:'./images/2W9A6692.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/2W9A8578A.jpg',
+            url:'./images/2W9A6570A.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/nkwo2W9A1218A.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/nkwo2W9A2290.jpg',
+            url:'./images/2W9A6751.avif',
             size:'horizontal'
         },
     ],
     [
         {
-            url:'./images/nkwo2W9A3083.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/nkwo2W9A3103.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/nkwo2W9A3540.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/nkwo2W9A3869.jpg',
-            size:'horizontal'
-        },
-        {
-            url:'./images/nkwoIMG_9998.jpg',
-            size:'horizontal'
-        },
-    ],
-    [
-        {
-            url:'./images/nkwo2W9A0883.jpg',
+            url:'./images/nkwo2W9A0828.avif',
             size:'vertical'
         },
         {
-            url:'./images/nkwoIMG_0202.jpg',
+            url:'./images/2W9A7685.avif',
+            size:'vertical'
+        },
+        {
+            url:'./images/nkwo2W9A0848.avif',
+            size:'vertical'
+        },
+    ],
+    [
+        {
+            url:'./images/2W9A6598.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/2W9A6692.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/2W9A8578A.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A1218A.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/nkwo2W9A2290.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A3083.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/nkwo2W9A3103.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A3540.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A3869.avif',
+            size:'horizontal'
+        },
+        {
+            url:'./images/nkwoIMG_9998.avif',
+            size:'horizontal'
+        },
+    ],
+    [
+        {
+            url:'./images/nkwo2W9A0883.avif',
+            size:'vertical'
+        },
+        {
+            url:'./images/nkwoIMG_0202.avif',
             size:'vertical'
         },
     ]
--- src/app/(pages)/event/wedding/page.tsx
+++ src/app/(pages)/event/wedding/page.tsx
@@ -10,28 +10,28 @@ function page() {
         { portalWeddingImages.map((items) => 
             items.length === 1 ? 
             items.map((images) => (
-                <div className='w-full p-1'><Image loading='lazy' placeholder='blur' src={require(`${images.url}`)} alt={images.url}/></div>
+                <div className='w-full p-1'><Image quality={100} loading='lazy' placeholder='blur' src={require(`${images.url}`)} alt={images.url}/></div>
             ))
             : items.length === 4 ? 
                 <div className='w-full flex flex-row px-4 md:px-10 sm:py-4'>
-                    <div className='p-1'><Image loading='lazy' placeholder='blur' src={require(`${items[0].url}`)} alt={items[0].url}/></div> 
+                    <div className='p-1'><Image quality={100} loading='lazy' placeholder='blur' src={require(`${items[0].url}`)} alt={items[0].url}/></div> 
                     <div className='flex flex-col items-center justify-between'>
                         {items.filter((ele) => ele.size === 'horizontal').map((horiImages) => (
-                            <Image className='h-1/2 w-3/4 p-1' loading='lazy' placeholder='blur' src={require(`${horiImages.url}`)} alt={horiImages.url}/>
+                            <Image quality={100} className='h-1/2 w-3/4 p-1' loading='lazy' placeholder='blur' src={require(`${horiImages.url}`)} alt={horiImages.url}/>
                         ))}
                     </div>
-                    <div className='p-1'><Image loading='lazy' placeholder='blur' src={require(`${items[items.length -1 ].url}`)} alt={items[items.length -1 ].url}/></div> 
+                    <div className='p-1'><Image quality={100} loading='lazy' placeholder='blur' src={require(`${items[items.length -1 ].url}`)} alt={items[items.length -1 ].url}/></div> 
                 </div>
             : items.length === 2 ? 
             <div className='w-full flex flex-row'>
                 {items.map((images) => (
-                    <div className='w-1/2 p-1'><Image loading='lazy' placeholder='blur' src={require(`${images.url}`)} alt={images.url}/></div>
+                    <div className='w-1/2 p-1'><Image quality={100} loading='lazy' placeholder='blur' src={require(`${images.url}`)} alt={images.url}/></div>
                 ))}
             </div>
             : items.length === 3 ? 
             <div className='w-full flex flex-row'>
                 {items.map((images) => (
-                    <div className='w-1/3 p-1'><Image loading='lazy' placeholder='blur' src={require(`${images.url}`)} alt={images.url}/></div>
+                    <div className='w-1/3 p-1'><Image quality={100} loading='lazy' placeholder='blur' src={require(`${images.url}`)} alt={images.url}/></div>
                 ))}
             </div>
              : '' 
